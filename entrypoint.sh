327239f7bd43db8a69c53e0ecedf5604ec798634
florferreyra <flor.sole.93@gmail.com>
2024-09-07 19:02:20 -0300

    fixed typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ cd app
 echo "Applying migrations..."
 python manage.py migrate --no-input
 
-# create superuser is doesn't exists
+# create superuser if doesn't exists
 python manage.py shell -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); \
 User.objects.create_superuser(username=os.environ.get('DJANGO_SUPERUSER_USERNAME'), \
 email=os.environ.get('DJANGO_SUPERUSER_EMAIL'), password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')) \
